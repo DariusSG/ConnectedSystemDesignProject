e77bc5293ad1994495d8fa24bf94f18b216aad86
dariussg <sg50koh@gmail.com>
2023-07-02 22:02:07 +0800

    Added Support for thread stop in event of disconnection from network Removed unused Flask dependency
    Reviewed-by: dariussg <sg50koh@gmail.com>

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
-from threading import Lock
+from threading import Lock, Event, Thread
 
-from flask import Flask, render_template
-from flask_socketio import SocketIO
+from socketio import Server, WSGIApp
 
-app = Flask(__name__)
-socketio = SocketIO(app)
+socketio = Server(async_mode='threading')
 
-thread = None
 thread_lock = Lock()
-
-SensorState = {}
+SensorState, clients = {}, []
 
 
 # SENSOR EVENT
@@ -38,34 +34,58 @@ def BBB4_Rx(RxData: dict):
 @socketio.event
 def BBB1_Rx(RxData: dict):
     global SensorState
-    pass
+    with thread_lock:
+        pass
 
+
+class ApplicationThread(Thread):
+    def __init__(self):
+        super(ApplicationThread, self).__init__()
+        self.daemon = True
+
+        self.stopSignal: Event = Event()
+
+    def run(self):
+        global SensorState
+        while self.stopSignal.is_set():
+            # Draw UI
+            # Handle Code
+            # Send Data to UI
+            socketio.emit("UI_Tx", {
+                "state": "",
+                "value": ""
+            })
+
+    def stop(self):
+        self.stopSignal.set()
+
+
+thread: ApplicationThread
 
 
 @socketio.event
-def connect():
+def connect(sid, environ, auth):
     global thread
     print('Connection established.')
     with thread_lock:
         if thread is None:
-            thread = socketio.start_background_task(application_thread)
+            thread = ApplicationThread()
+            thread.start()
+        if sid not in clients:
+            clients.append(sid)
 
 
-def application_thread():
-    global SensorState
-    # Draw UI
-    # Handle Code
-    # Send Data to UI
-    socketio.emit('UI_Tx', {
-        'state': "",
-        'value': ""
-    })
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
+@socketio.event
+def disconnect(sid):
+    global thread
+    if sid in clients:
+        clients.remove(sid)
+    if len(clients) == 0:
+        thread.stop()
 
 
 if __name__ == '__main__':
-    app.run(host='192.168.12.1')
+    app = WSGIApp(socketio)
+    import eventlet
+    from eventlet.wsgi import server as eventlet_wsgi_server
+    eventlet_wsgi_server(eventlet.listen(('192.168.12.1', 8000)), app)
